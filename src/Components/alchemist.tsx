0e5772c4b24d58faa038387254a0091319651def
Katanaco <jonathan.schmitt@colorado.edu>
2022-02-13 22:43:21 -0700

    json

--- src/Components/alchemist.tsx
+++ src/Components/alchemist.tsx
@@ -11,13 +11,25 @@ interface AlchemistState {
 
 export default class Alchemist extends Component<AlchemistProps, AlchemistState> {
     render() {
+        console.log(data);
         return (
             <div>
                 <h1>What can I brew for you?</h1>
                 <div>
                     {this.props.name}
-                    
                 </div>
+                <ul>
+                    {data.map(item =>
+                        <li>
+                            <div>
+                                {item.name}
+                            </div>
+                            <div>
+                                {item.email}
+                            </div>
+                        </li>
+                    )}
+                </ul>
             </div>
         )
     }
